f40a1aed9e8fff485a9bfc0cbaf807d9243649e6
devblackie <newtieblackie@gmail.com>
2022-06-05 17:11:07 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(num1, num2) {
+    return (num1 + num2)
+}
+function subtract(num1, num2) {
+    return (num1 - num2)
+}
+function multiply(num1, num2) {
+    return (num1 * num2)
+}
+function divide(num1, num2) {
+    return (num1 / num2)
+}
+function increment(num1, num2) {
+    return (num1 + 1)
+}
+function decrement(num1, num2) {
+    return (num1 - 1)
+}
+//  others  
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
